a07637b35fccaeba23d2fb9e8344a86dc40548f4
Jesse_V <jvictors@jessevictors.com>
2013-08-02 17:57:09 -0800

    Trajectory classes

---
+++ src/Trajectory/Trajectory.hpp
@@ -0,0 +1,11 @@
+
+/*
+    Trajectory
+        has topology
+            has list of atoms
+                each has symbol, charge, radius, mass, and atomic number
+            has list of bond
+                each has pair of indexes, referring to connected atoms
+        has list of snapshots
+            each has list of xyz coordinates for each atom
+*/
