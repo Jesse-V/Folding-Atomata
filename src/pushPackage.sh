4e05cc0abececa1bf78a629e63601ffb03eb6ead
Jesse_V <jvictors@jessevictors.com>
2013-08-22 23:08:32 -0800

    successful Launchpad PPA build for #15

--- src/pushPackage.sh
+++ src/pushPackage.sh
@@ -2,24 +2,22 @@
 
 ./clean.sh
 
+name="foldingatomata_0.4.3"
+
 cd ..
-tar -czf foldingatomata_0.05.orig.tar.gz src/ #http://xkcd.com/1168/
+tar -czf ${name}.orig.tar.gz src/ #http://xkcd.com/1168/
 cd src/
 echo "Successfully made tarball."
 
-dpkg-buildpackage -kC20BEC80
-echo "Successfully made Debian package."
-
-dput ppa:jvictors/testing ../foldingatomata_0.05-0ppa1_amd64.changes
-echo "Successfully uploaded packages to Launchpad PPA."
+dpkg-buildpackage -S -sa -kC20BEC80
+echo "Successfully made signed Debian package."
 
 cd ..
-rm -f foldingatomata_0.05.orig.tar.gz foldingatomata_0.05-0ppa1.debian.tar.gz
-rm -f foldingatomata_0.05-0ppa1.dsc foldingatomata_0.05-0ppa1_amd64.changes
-rm -f foldingatomata_0.05-0ppa1_amd64.ppa.upload
-cd src/
-rm -rf obj-x86_64-linux-gnu/
-rm -rf debian/foldingatomata/
-echo "Successfully cleaned package build files and folders."
+dput ppa:jvictors/testing ${name}_source.changes
+
+rm -f ${name}.orig.tar.gz ${name}.debian.tar.gz
+rm -f ${name}.dsc ${name}_amd64.changes ${name}_source.changes.changes
+rm -f ${name}_source.ppa.upload
+echo "Successfully cleaned package build files and folders." #debuild clean
 
 echo "Done pushing packages. Check your email for Launchpad build reports."
