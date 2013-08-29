8f454b6f7f1e662ad631a8928713fdda590b0a91
Jesse_V <jvictors@jessevictors.com>
2013-08-29 14:23:34 -0600

    three textures for #3, working gitInstall script

--- gitInstall.sh
+++ gitInstall.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
+
+#build
+sudo apt-get install cmake libglew-dev freeglut3-dev build-essential
 cd src
 cmake .
-if (make) then
-	sudo make install
+cpus=$(grep -c ^processor /proc/cpuinfo)
+
+#attempt to compile
+if (make -j $cpus) then
+
+    #setup
+    cp -rl ../debian/ debian/
+    cd debian/extra_includes/
+    gzip --best -c manpage > FoldingAtomata.1.gz
+    cd ../../
+
+    #install
+    sudo make install
+    echo "Successfully installed Folding Atomata on the filesystem."
+
+    #clean
+    echo "Cleaning up..."
+    rm -rf debian/
+    ./clean.sh
+    echo "... done. Stop."
 fi
